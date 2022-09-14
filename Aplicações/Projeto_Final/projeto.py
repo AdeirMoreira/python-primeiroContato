823cccd534559e61bd4108c4998f82ff0a17c213
AdeirMoreira <adeir.maia@hotmail.com>
2022-09-14 10:21:17 -0300

    Feat: Projeto final grafico covid concluido

--- Aplicações/Projeto_Final/projeto.py
+++ Aplicações/Projeto_Final/projeto.py
@@ -1,6 +1,7 @@
 import csv
 import requests as r
 import datetime as dt
+from PIL import Image
 
 url = 'http://api.covid19api.com/dayone/country/brazil'
 response = r.get(url).json()
@@ -21,16 +22,83 @@ CONFIRMADOS = 0
 OBITOS = 1
 RECUPERADOS = 2
 ATIVOS = 3
-DATA = 4
+DATE = 4
 
 for i in range(1, len(dadosUteis)):
-    dadosUteis[i][DATA] = dadosUteis[i][DATA][:10]
+    dadosUteis[i][DATE] = dadosUteis[i][DATE][:10]
 
 with open('Aplicações/Projeto_Final/brasil_covid.csv', 'w') as arquivo: 
     escrever = csv.writer(arquivo)
     escrever.writerows(dadosUteis)
 
 for i in range(1, len(dadosUteis)):
-    dadosUteis[i][DATA] = dt.datetime.strptime(dadosUteis[i][DATA], '%Y-%m-%d')
-print(dadosUteis)
+    dadosUteis[i][DATE] = dt.datetime.strptime(dadosUteis[i][DATE], '%Y-%m-%d')
+# print(dadosUteis)
+
+def get_dataSets(y,labels):
+    if type(y[0]) == list:
+        datasets = []
+        for i in range(len(y)):
+            datasets.append({
+                'label' : labels[i],
+                'data' : y[i]
+            })
+        return datasets
+    else:
+        return [
+            {
+                'label' : labels[0],
+                'data' : y
+            }
+        ]
+
+def setTitle (title = ''):
+    if title != '':
+        display = 'true'
+    else:
+        display = 'false'
+    return {
+        'title' : title,
+        'display' : display
+    }
+
+def create_chart(x,y,labels, type='bar',title=''):
+    datasets = get_dataSets(y,labels)
+    options = setTitle(title)
+    chart = {
+        'type': type,
+        'data': {
+            'labels' : x,
+            'datasets' : datasets
+        },
+        'options' : options
+    }
+    return chart
+
+def get_API_Chart(chart):
+    baseURL = 'https://quickchart.io/chart'
+    response = r.get(f'{baseURL}?c={str(chart)}')
+    return response.content
+
+def save_image(path, content):
+    with open(path , 'wb') as image:
+        image.write(content)
+
+y_data1 = []
+for data in dadosUteis[1::30]:
+    y_data1.append(data[CONFIRMADOS])
+
+y_data2 = []
+for data in dadosUteis[1::30]:
+    y_data2.append(data[OBITOS])
+
+labels = ['Confirmados', 'Óbitos']
+
+x = []
+for data in dadosUteis[1::30]:
+    x.append(data[DATE].strftime('%d/%m/%Y'))
+
+chart = create_chart(x,[y_data1,y_data2],labels,title='Gráfico Confirmados X Óbitos')
+chart_content = get_API_Chart(chart)
+save_image('Aplicações/Projeto_Final/graficoImg.png', chart_content)
 
